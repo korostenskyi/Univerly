61bfbaff6b027a236d100949c0cf1a007e1874b8
Roman Korostenskyi <romankorostensky@gmail.com>
2019-05-17 00:46:53 +0300

    Add base structure of university database

---
+++ SQL/University.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS university;
+
+USE university;
+
+CREATE TABLE IF NOT EXISTS department(
+	id INT NOT NULL AUTO_INCREMENT,
+
+	department_name VARCHAR(45) NOT NULL,
+    head_of_department_name VARCHAR(45) NOT NULL,
+	
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS lector(
+	id INT NOT NULL AUTO_INCREMENT,
+    
+    lector_name VARCHAR(45) NOT NULL,
+    degree ENUM('assistant', 'assosiate professor', 'professor') NOT NULL,
+    salary INT NOT NULL,
+    department_id INT,
+    
+    FOREIGN KEY (id) REFERENCES department(id),
+    PRIMARY KEY (id)
+);
